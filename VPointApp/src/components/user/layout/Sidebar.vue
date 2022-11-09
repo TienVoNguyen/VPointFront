43a2798f905d83f14afaea8218bbca79756d2803
CanhNguyen <leagueyasuo186@gmail.com>
2022-11-09 09:09:55 +0700

    done

--- VPointApp/src/components/user/layout/Sidebar.vue
+++ VPointApp/src/components/user/layout/Sidebar.vue
@@ -81,64 +81,15 @@
         </a>
       </nav>
     </nav>
-
-    <el-dialog :append-to-body="true" :visible.sync="dialogFormVisible" width="30%" class="text-center">
-      <span slot="title" style="width: 214px;
-height: 42px;
-left: 230px;
-top: 100px;
-font-family: 'Roboto';
-font-style: normal;
-font-weight: 700;
-font-size: 30px;
-line-height: 42px;
-color: #246CD9;">Đổi mật khẩu</span>
-      <el-form>
-        <el-form-item label="Nhập mật cũ">
-          <el-input v-model="changePass.oldPassword" type="password" autocomplete="off" show-password></el-input>
-          <small v-if="oldPass != null" style="color: red">{{oldPass}}</small>
-        </el-form-item>
-        <el-form-item label="Nhập mật khẩu mới">
-          <el-input v-model="changePass.newPassword" type="password" autocomplete="off" show-password></el-input>
-          <small v-if="errP1 != null" style="color: red">{{errP1}}</small>
-        </el-form-item>
-        <el-form-item label="Xác nhận mật khẩu mới">
-          <el-input v-model="changePass.confirmNewPassword" type="password" autocomplete="off" show-password></el-input>
-          <small v-if="errorsPass != null" style="color: red">{{errorsPass}}</small>
-        </el-form-item>
-      </el-form>
-      <span slot="footer" class="dialog-footer">
-    <el-button type="primary" @click.prevent="RepassUser(currentUser.id)">Confirm</el-button>
-        <!--        <pre>{{changePass}}</pre>-->
-  </span>
-    </el-dialog>
-
   </div>
 
 </template>
 <script>
-import authService from "@/service/auth-service";
-import swal from "sweetalert2";
-
 export default {
   name: "SidebarComponent",
   data: function () {
     return {
-      dialogTableVisible: false,
-      dialogFormVisible: false,
-      oldPass: '',
-      errorsPass: '',
-      errP1: '',
-      check1: true,
-      check: true,
-      checkNewPass: true,
       formLabelWidth: '120px',
-      changePass: {
-        oldPassword: '',
-        newPassword: '',
-        confirmNewPassword: '',
-
-      },
     };
 
   },
@@ -157,76 +108,10 @@ export default {
     handleChangePassLayout() {
       this.$emit('clickOpenChangePass');
     },
-    removeValidate1(check) {
-      this.dialogFormVisible = check
-      this.oldPass = ''
-      this.errP1 = ''
-      this.errorsPass = ''
-    },
-    validPass: function (pass) {
-      var re = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/;
-      return re.test(pass);
-    },
     logOut() {
       this.$store.dispatch('auth/logout');
       this.$router.push('/login');
     },
-    RepassUser(userId) {
-      if (!this.changePass.newPassword && this.changePass.confirmNewPassword || !this.changePass.newPassword && !this.changePass.confirmNewPassword) {
-        this.errP1 = 'Vui lòng nhập mật khẩu mới'
-        this.checkNewPass = false;
-      } else if (!this.validPass(this.changePass.newPassword)) {
-        this.errP1 = 'Mật khẩu gồm 8 ký tự trở lên có ít nhất một số và một chữ hoa và chữ thường'
-        this.checkNewPass = false
-      } else {
-        this.errP1 = ''
-        this.checkNewPass = true;
-      }
-      if (this.changePass.newPassword && this.changePass.confirmNewPassword && !this.changePass.oldPassword){
-        this.oldPass = 'Nhập mật khẩu cũ'
-        this.check = false;
-      } else if (this.changePass.newPassword && this.changePass.confirmNewPassword && this.changePass.oldPassword){
-        this.oldPass = ''
-        this.check = true;
-      }
-      if (this.changePass.newPassword && !this.changePass.confirmNewPassword) {
-        this.errorsPass = 'Vui lòng xác nhận mật khẩu mới'
-        this.check1 = false;
-      } else if (this.changePass.newPassword !== this.changePass.confirmNewPassword) {
-        this.errorsPass = 'Mật khẩu không trùng khớp'
-        this.check1 = false;
-      } else if (this.changePass.newPassword === this.changePass.confirmNewPassword) {
-        this.errorsPass = ''
-        this.check1 = true;
-      }
-      if (this.check1 === true && this.check === true && this.checkNewPass === true) {
-        authService.userRepass(userId, this.changePass)
-            .then(
-                async data => {
-                  this.a = data.message;
-                  if(this.changePass.oldPassword === this.changePass.newPassword){
-                    this.dialogFormVisible1 = true;
-                    this.errP1 = 'Trùng với mật khẩu cũ'
-                  } else {
-                    this.errP1 = ''
-                    this.dialogFormVisible1 = false;
-                    await swal.fire({
-                          toast: true,
-                          title: "Xong!",
-                          icon: "success",
-                          position: 'top-end',
-                          showConfirmButton: false,
-                          timer: 3000
-                        }
-                    )
-                  }
-                },
-                () => {
-                  this.oldPass = 'Mật khẩu cũ không chính xác'
-                  this.dialogFormVisible = true;
-                });
-      }
-    }
   }
 }
 </script>
